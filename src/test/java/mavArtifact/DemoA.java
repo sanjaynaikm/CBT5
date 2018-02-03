7e709a2c64e29edc41c47b15be4ea46ff0c761e8
CBT <sanjaynaikm@gmail.com>
2018-02-03 16:38:02 +0530

    my first commit

---
+++ src/test/java/mavArtifact/DemoA.java
@@ -0,0 +1,32 @@
+package mavArtifact;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class DemoA 
+{
+	
+	@Test
+	public void testA() throws MalformedURLException
+	{
+		/*
+		URL r = new URL("http://loclahost:4444/wd/hub");
+		DesiredCapabilities d = new DesiredCapabilities();
+		d.setBrowserName("firefox");
+		WebDriver driver = new RemoteWebDriver(r,d);
+		driver.quit();
+		*/
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.flagroot.com");
+		//driver.get("http://www.facebook.com");
+		
+	}
+
+}
