706dfd4b5e13fe3f58e6f5a42115914c2b5762aa
sanjaynaikm <34195235+sanjaynaikm@users.noreply.github.com>
2018-02-04 15:45:32 +0530

    Update DemoA.java

--- src/test/java/mavArtifact/DemoA.java
+++ src/test/java/mavArtifact/DemoA.java
@@ -25,6 +25,7 @@ public class DemoA
 		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.flagroot.com");
+		driver.findElement(By.linkText("Home"));
 		System.out.println("Hello ");
 		//driver.get("http://www.facebook.com");
 		
